1c5be65d24676f68335c0cec4ac13184f484dc80
Edmond <Edmond.Akpan@gmail.com>
2019-07-03 18:07:12 +0900

    Updates

---
+++ precache-manifest.deb82df2abad8330b703ab05d85e4022.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64f838853eeadebd198aad1c9294527",
+    "url": "/autocomplete-search/index.html"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/autocomplete-search/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "f521106017c8939beabd",
+    "url": "/autocomplete-search/static/js/main.daa431aa.chunk.js"
+  },
+  {
+    "revision": "da8b9219aa421fcc7118",
+    "url": "/autocomplete-search/static/js/runtime~main.5d6f9b56.js"
+  }
+]);
