803d15f8cfbcad769b18442ef79bd8f52ef79abe
Edmond <Edmond.Akpan@gmail.com>
2019-07-03 18:05:59 +0900

    updated package scripts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Search from './lib/Search';
 import { cityNames } from './example/data.js';
